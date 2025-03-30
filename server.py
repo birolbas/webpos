1920acee96cad7f2376a71b7c1c061bfc6c4df4c
birolbas <bbasbirol@hotmail.com>
2025-03-30 16:34:14 +0300

    update

--- server.py
+++ server.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def lobby():
+    return render_template("login.html")
+@app.route("/order")
+def order():
     date = dt.datetime.now()
     formatted_date = date.strftime("%d %B %Y")
-    formatted_time = date.strftime("%H:%M:%S")
+    formatted_time = date.strftime("%H:%M")
     day = date.strftime("%A")  
     return render_template("order.html", date = formatted_date, time = formatted_time, day=day)
 
