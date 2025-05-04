4b6e186de81f2da75b31ecfa176ca68735670b23
birolbas <bbasbirol@hotmail.com>
2025-05-04 23:27:04 +0300

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,18 @@
-function appendOrder(){
-    const menuList = document.getElementsByClassName("order-items")[0]; // İlk öğeye erişmek için [0] ekliyoruz
-    console.log(menuList);
+function appendOrder(button){
+    const menuList = document.getElementsByClassName("order-items")[0]; 
     
+    let chosen_product = button
+    let product_name = chosen_product.getElementsByClassName("product-name")[0].innerHTML;
+    product_price = chosen_product.getElementsByClassName("product-price")[0].innerHTML;
+    product_price = parseFloat(product_price);
+
     const newItem = document.createElement("li");
-    newItem.innerHTML = `<span class="item-name">ASD</span><span class="price">0 TL</span>`;
-    console.log(newItem);
+    newItem.innerHTML = `<span class="item-name">${product_name}</span><span class="price">${product_price}</span>`;
+    menuList.appendChild(newItem);
     
-    menuList.appendChild(newItem); // Artık appendChild geçerli
+    total_price = document.getElementsByClassName("order-summary")[0].getElementsByClassName("total-price")[0].innerHTML;
+    total_price = parseFloat(total_price);  
+    total_price = total_price + product_price;
+    document.getElementsByClassName("order-summary")[0].getElementsByClassName("total-price")[0].innerHTML = total_price;
+
 }
